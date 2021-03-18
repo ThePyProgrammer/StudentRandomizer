02c69a85dc8d852283b793772f031e1f6b1a68bf
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-18 23:12:35 +0800

    Delete IAuthenticationHelperCreatedListener.kt

--- app/src/main/java/com/thepyprogrammer/randstudent/model/IAuthenticationHelperCreatedListener.kt
+++
@@ -1,9 +0,0 @@
-package com.thepyprogrammer.randstudent.model
-
-import com.microsoft.identity.client.exception.MsalException
-
-
-interface IAuthenticationHelperCreatedListener {
-    fun onCreated(authHelper: AuthenticationHelper?)
-    fun onError(exception: MsalException?)
-}
