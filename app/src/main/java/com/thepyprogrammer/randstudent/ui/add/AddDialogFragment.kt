45708f2d6147aec5876d3cb0c2d41d3769a5fe92
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-19 13:22:23 +0800

    Create AddDialogFragment.kt

---
+++ app/src/main/java/com/thepyprogrammer/randstudent/ui/add/AddDialogFragment.kt
@@ -0,0 +1,44 @@
+package com.thepyprogrammer.randstudent.ui.add
+
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RatingBar
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.snackbar.Snackbar
+import com.thepyprogrammer.randstudent.R
+import java.time.LocalDate
+
+open class AddDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("Create List")
+        // set the custom layout
+        val customLayout: View = LayoutInflater.from(activity?.applicationContext).inflate(
+            R.layout.expanded_item_todo,
+            null
+        )
+        builder.setView(customLayout)
+        // create and show the alert dialog
+
+
+
+        builder.setPositiveButton(
+            "OK"
+        ) { _, _ ->
+
+        }
+
+        val dialog: AlertDialog = builder.create()
+        return dialog
+    }
+}
