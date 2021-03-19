6928ffeb96cfe6d7791f8e0b33617e10deb59a4c
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-20 00:21:11 +0800

    Update StudentList.kt

--- app/src/main/java/com/thepyprogrammer/randstudent/model/list/StudentList.kt
+++ app/src/main/java/com/thepyprogrammer/randstudent/model/list/StudentList.kt
@@ -2,7 +2,8 @@ package com.thepyprogrammer.randstudent.model.list
 
 data class StudentList(
     var title: String,
-    var students: MutableList<Student>
+    var students: MutableList<Student>,
+    var selected: Boolean
 ) {
     override fun toString(): String {
         return title
