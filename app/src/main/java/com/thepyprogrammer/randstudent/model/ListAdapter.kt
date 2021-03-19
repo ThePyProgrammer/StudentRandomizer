6534d7d0e6582f8fc66bd3b3988e339d2ed7eec7
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-19 13:38:12 +0800

    Update ListAdapter.kt

--- app/src/main/java/com/thepyprogrammer/randstudent/model/ListAdapter.kt
+++ app/src/main/java/com/thepyprogrammer/randstudent/model/ListAdapter.kt
@@ -1,23 +1,67 @@
 package com.thepyprogrammer.randstudent.model
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.thepyprogrammer.randstudent.R
+import com.thepyprogrammer.randstudent.model.list.StudentList
+import com.thepyprogrammer.randstudent.ui.AppMainActivity
 
-class ListAdapter: RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+class ListAdapter(
+    private val activity: AppMainActivity,
+    val lists: MutableList<StudentList> = mutableListOf()
+): RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+
+    var recentlyDeleted: StudentList? = null
+    var recentlyDeletedPosition: Int = -1
 
     class ListViewHolder(itemView: CardView) : RecyclerView.ViewHolder(itemView)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder = ListViewHolder(
+        LayoutInflater.from(parent.context).inflate(
+            R.layout.item_todo,
+            parent,
+            false
+        ) as CardView)
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return lists.size
+    }
+
+    fun deleteItem(position: Int) {
+        lists.removeAt(position)
+        notifyDataSetChanged()
+        val view: View = activity.findViewById(R.id.home)
+        val snackbar: Snackbar = Snackbar.make(
+            view, "Task Has Been Deleted",
+            Snackbar.LENGTH_LONG
+        )
+        snackbar.setAction("UNDO") { undoDelete() }
+        snackbar.show()
+        //activity.updateFile()
+    }
+
+    private fun undoDelete() {
+        recentlyDeleted?.let {
+            todos.add(
+                recentlyDeletedPosition,
+                it
+            )
+            notifyItemInserted(recentlyDeletedPosition)
+            val view: View = activity.findViewById(R.id.home)
+            val snackbar: Snackbar = Snackbar.make(
+                view, "Task Has Been Restored",
+                Snackbar.LENGTH_SHORT
+            )
+            snackbar.show()
+        }
     }
 
 
