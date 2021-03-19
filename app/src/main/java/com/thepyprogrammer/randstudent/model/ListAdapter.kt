8f2c77265979aeed4bf55f8fc0a91039555227a8
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-19 13:22:15 +0800

    Create ListAdapter.kt

---
+++ app/src/main/java/com/thepyprogrammer/randstudent/model/ListAdapter.kt
@@ -0,0 +1,24 @@
+package com.thepyprogrammer.randstudent.model
+
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+
+    class ListViewHolder(itemView: CardView) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
