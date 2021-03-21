6a8118aabae72197f565693939fd2d8be053901a
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-21 15:33:34 +0800

    Update AddListDialogFragment.kt

--- app/src/main/java/com/thepyprogrammer/randstudent/ui/add/AddListDialogFragment.kt
+++ app/src/main/java/com/thepyprogrammer/randstudent/ui/add/AddListDialogFragment.kt
@@ -1,21 +1,12 @@
 package com.thepyprogrammer.randstudent.ui.add
 
 import android.app.AlertDialog
-import android.app.DatePickerDialog
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.RatingBar
 import androidx.fragment.app.DialogFragment
-import com.google.android.material.snackbar.Snackbar
 import com.thepyprogrammer.randstudent.R
-import java.time.LocalDate
 
 open class AddListDialogFragment : DialogFragment() {
 
@@ -38,7 +29,6 @@ open class AddListDialogFragment : DialogFragment() {
 
         }
 
-        val dialog: AlertDialog = builder.create()
-        return dialog
+        return builder.create()
     }
 }
